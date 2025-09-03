4bcd23d6ad03b0fd6099da49f51d7830fbcde5f3
i.shulika <i.shulika@pluscards.ru>
2025-09-03 23:17:55 +0300

    feat: implement XmlDataProcessorService for adding sum attributes

--- src/TestTaskXmlReportApp/TestTaskXmlReportApp/Services/XmlDataProcessorService.cs
+++ src/TestTaskXmlReportApp/TestTaskXmlReportApp/Services/XmlDataProcessorService.cs
@@ -1,15 +1,49 @@
-﻿namespace TestTaskXmlReportApp.Services
+﻿using System.Xml.Linq;
+using System.Globalization;
+
+namespace TestTaskXmlReportApp.Services
 {
     public class XmlDataProcessorService : IXmlDataProcessorService
     {
         public void AddTotalSalaryAttribute(string xmlPath)
         {
-            // Добавление атрибутов первой вариации
+            XDocument doc = XDocument.Load(xmlPath);
+
+            foreach (var employeeElem in doc.Descendants("Employee"))
+            {
+                decimal salarySum = 0;
+                foreach (var salaryElem in employeeElem.Elements("salary"))
+                {
+                    string amountValue = salaryElem.Attribute("amount")?.Value;
+                    if (decimal.TryParse(amountValue, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal amount))
+                    {
+                        salarySum += amount;
+                    }
+                }
+
+                employeeElem.SetAttributeValue("salary_sum", salarySum.ToString(CultureInfo.InvariantCulture));
+            }
+
+            doc.Save(xmlPath);
         }
 
         public void AddTotalAmountToData1(string data1Path)
         {
-            // Добавление атрибутов второй вариации
+            XDocument doc = XDocument.Load(data1Path);
+
+            decimal amountSum = 0;
+            foreach (var itemElem in doc.Descendants("item"))
+            {
+                string amountValue = itemElem.Attribute("amount")?.Value;
+                if (decimal.TryParse(amountValue, NumberStyles.Any, CultureInfo.InvariantCulture, out decimal amount))
+                {
+                    amountSum += amount;
+                }
+            }
+
+            doc.Root?.SetAttributeValue("amount_sum", amountSum.ToString(CultureInfo.InvariantCulture));
+
+            doc.Save(data1Path);
         }
     }
 }
